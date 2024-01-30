fdf1390934754358d0a1338916164947d6682783
Sunny <sunnysideupdowntown@gmail.com>
2024-01-30 19:35:02 +0900

    ViewController들 추가

---
+++ StoryboardInstantiatingTest/ViewControllers/BlueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BlueViewController.swift
+//  StoryboardInstantiatingTest
+//
+//  Created by YOUNGSUN on 1/30/24.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ StoryboardInstantiatingTest/ViewControllers/RedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RedViewController.swift
+//  StoryboardInstantiatingTest
+//
+//  Created by YOUNGSUN on 1/30/24.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
