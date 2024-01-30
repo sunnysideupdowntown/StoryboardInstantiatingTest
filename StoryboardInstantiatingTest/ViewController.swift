47c8fcc7de9834997311cdbb48c69f72e4a470b7
Sunny <sunnysideupdowntown@gmail.com>
2024-01-30 19:32:42 +0900

    Initial Commit

---
+++ StoryboardInstantiatingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardInstantiatingTest
+//
+//  Created by YOUNGSUN on 1/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
