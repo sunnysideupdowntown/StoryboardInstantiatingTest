9c58ac9a75ed495a3673a2229053b7131486d567
Sunny <sunnysideupdowntown@gmail.com>
2024-01-30 20:08:01 +0900

    2. 코드로 ViewController Instantiate 하기

--- StoryboardInstantiatingTest/ViewController.swift
+++ StoryboardInstantiatingTest/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func redButtonTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "RedViewController", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "RedViewController")
+        let redViewController = viewController as! RedViewController
+        self.present(redViewController, animated: true)
+    }
 
+    @IBAction func blueButtonTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "BlueViewController", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "BlueViewController")
+        let blueViewController = viewController as! BlueViewController
+        self.present(blueViewController, animated: true)
+    }
 }
 
